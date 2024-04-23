8fcda31e93975ac0d0f45755fdb174dcfd1b6650
ranst91 <ran.shem.tov@lemonade.com>
2024-04-23 07:04:10 +0200

    feat: add dev mode

--- rollup.config.js
+++ rollup.config.js
@@ -10,29 +10,36 @@ const entries = [
     './src/matchers/index.ts',
 ]
 
-export default {
-    input: entries, // Pointing to your TypeScript entry file
-    output: [
-        {
-            dir: 'dist',
-            entryFileNames: '[name].mjs',
-            chunkFileNames: '[name]-[hash].mjs',
-            format: 'esm',
-        },
-        {
-            dir: 'dist',
-            entryFileNames: '[name].js',
-            chunkFileNames: '[name]-[hash].js',
-            format: 'cjs',
-        },
-    ],
-    external: id =>
-        !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/'),
-    plugins: [
+export default CLIArgs => {
+    const plugins = [
         del({ targets: 'dist/*' }),
         typescript(), // Compiles TypeScript to JavaScript
         resolve(),    // Resolves node_modules imports
         commonjs(),   // Converts CommonJS modules to ES6
-        terser()      // Minify the output (optional)
-    ]
+    ];
+    if (CLIArgs.mini) {
+      plugins.push(terser()); // Minify the output (optional)
+    }
+    return {
+        input: entries, // Pointing to your TypeScript entry file
+        output: [
+            {
+                dir: 'dist',
+                entryFileNames: '[name].mjs',
+                chunkFileNames: '[name]-[hash].mjs',
+                format: 'esm',
+                sourcemap: CLIArgs.sourcemaps,
+            },
+            {
+                dir: 'dist',
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name]-[hash].js',
+                format: 'cjs',
+                sourcemap: CLIArgs.sourcemaps,
+            },
+        ],
+        external: id =>
+            !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/'),
+        plugins
+    }
 };
